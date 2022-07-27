18328a2ebcad7e625f86e5ea8def782c66fc0a40
Edoardo <edoardogamurrini@gmail.com>
2022-07-27 12:04:54 +0200

    home mobile

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 // change logo color when the navbar is black
-window.addEventListener("scroll", (event) => {
-    let scrollY = this.scrollY;
+// window.addEventListener("scroll", (event) => {
+//     let scrollY = this.scrollY;
     
-    // if (scrollY >= 100){
-    //   $("#logo").css({
-    //     "display": "none",
-    //   });
-      // if (screen.width <= 1100 ){
-      //   $("#logo-white").css({
-      //       "display": "inline",
-      //       "width" : "23%"
-      //     });
-      // } else{
-      //   $("#logo-white").css({
-      //       "display": "inline",
-      //       "width" : "10%"
-      //     });
-      // }
+//     if (scrollY >= 100){
+//       $("#logo").css({
+//         "display": "none",
+//       });
+//       if (screen.width <= 1100 ){
+//         $("#logo-white").css({
+//             "display": "inline",
+//             "width" : "23%"
+//           });
+//       } else{
+//         $("#logo-white").css({
+//             "display": "inline",
+//             "width" : "10%"
+//           });
+//       }
       
-    // } else if (scrollY < 100 || scrollY == 0){
-    //     $("#logo-white").css({
-    //       "display": "none",
-    //     });
-    //     $("#logo").css({
-    //       "display": "inline",
-    //     });
-    //   }
-  });
+//     } else if (scrollY < 100 || scrollY == 0){
+//         $("#logo-white").css({
+//           "display": "none",
+//         });
+//         $("#logo").css({
+//           "display": "inline",
+//         });
+//       }
+//   });
   
   // transition of the hamburger button
   function myFunction(x) {
